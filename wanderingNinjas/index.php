66912c8f01b42961b191da58303f4cede0f6b3c9
AcKhyrine <126878601+AcKhyrine@users.noreply.github.com>
2024-08-26 15:49:19 +0800

    Add files via upload

---
+++ wanderingNinjas/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wandering Ninjas</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container">
+    <img class="mainImg" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eaboutus1.svg">
+    <div class="allText aboveText">
+      <p class="text-blk headingText">
+      ABOUT US
+      </p>
+      <p class="text-blk subHeadingText">
+        Our Mission and Vision
+      </p>
+      <p class="text-blk description">
+      At Wander Travel Agency, we excel in providing exceptional travel experiences through our diverse range of tours, flights, and hotel bookings. Our dedicated team prioritizes customer satisfaction, ensuring unforgettable journeys for every traveler. With a focus on expanding our customer base, we are committed to delivering top-notch service and creating lasting memories for all our clients.
+     </p>
+      <button class="explore">
+        Learn More
+      </button>
+    </div>
+  </div>
+</div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
+</html>
